09bcd58696f4897f299f1061a9d7009d6c8f8947
Charles Lambert <charles@mbp-de-charles.lan>
2022-08-03 15:31:08 +0200

    add route characters

--- server.js
+++ server.js
@@ -9,16 +9,39 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-//imports de nos routes
-const comicsRoutes = require("./routes/comics");
-const characterRoutes = require("./routes/characters");
-app.use(comicsRoutes);
-app.use(characterRoutes);
+// //imports de nos routes
+// const comicsRoutes = require("./routes/comics");
+// const characterRoutes = require("./routes/characters");
+// app.use(comicsRoutes);
+// app.use(characterRoutes);
+
+app.get("/comics", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.API_KEY}`
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+router.get("/characters", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}`
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 
 app.all("*", (req, res) => {
   res.status(404).json({ error: error.message });
 });
 
-app.listen(process.env.PORT || 4000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server has started ✌️");
 });
