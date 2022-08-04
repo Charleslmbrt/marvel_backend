0ba834f7f0721c14b7b7d1360a58b1c1d06f7525
Charles Lambert <charles@mbp-de-charles.lan>
2022-08-04 14:23:47 +0200

    add req params name

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get("/comics", async (req, res) => {
 app.get("/characters", async (req, res) => {
   try {
     const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}`
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}&name=${req.params.name}`
     );
     res.json(response.data);
   } catch (error) {
